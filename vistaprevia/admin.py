888fa9e8aac281118d60560b153c414055d85b20
fede <federico.e.hernandez@gmail.com>
2023-03-01 22:57:03 -0300

    Just commit

--- vistaprevia/admin.py
+++ vistaprevia/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from vistaprevia.models import Producto
+from vistaprevia.models import Categoria
+
 
 # Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+    fields = ["fecha_publicacion", "producto", "imagen", "categoria"]
+
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Categoria)
--- vistaprevia/models.py
+++ vistaprevia/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return "%s" % self.nombre
+
+
+class Producto(models.Model):
+    producto = models.CharField(max_length=200)
+    fecha_publicacion = models.DateTimeField("Fecha de publicación")
+    imagen = models.ImageField(upload_to="producto/%Y/%m/%d", blank=True, null=True)
+    categoria = models.ManyToManyField(Categoria)
+
+    def __str__(
+        self,
+    ):
+        return self.producto + "---" + str(self.fecha_publicacion)
